af0d5a096648a78989a46a696d4de23344051e86
Sergio Vavra <sergio.vavra@gmail.com>
2024-07-15 21:31:00 +0200

    Added some more code

--- practice.py
+++ practice.py
@@ -1,33 +1,62 @@
-# import numpy as np
-# import time
+import numpy as np
+import time
 
-# a = np.random.rand(1000000)
-# b = np.random.rand(1000000)
+a = np.random.rand(1000000)
+b = np.random.rand(1000000)
 
-# tic = time.time()
-# c = np.dot(a, b)
-# toc = time.time()
+tic = time.time()
+c = np.dot(a, b)
+toc = time.time()
 
-# print(c)
-# print("This took:" + str((toc - tic) * 1000) + "ms")
+print(c)
+print("This took:" + str((toc - tic) * 1000) + "ms")
 
-# c = 0
-# tic = time.time()
-# for i in range(1000000):
-#     c += a[i]*b[i]
-# toc = time.time()
-# print(c)
-# print("This took:" + str((toc - tic) * 1000) + "ms")
+c = 0
+tic = time.time()
+for i in range(1000000):
+    c += a[i]*b[i]
+toc = time.time()
+print(c)
+print("This took:" + str((toc - tic) * 1000) + "ms")
 
-# d = np.array([[50, 0, 4, 20],
-#              [1.2, 104, 58, 2.0],
-#              [1.3, 138, 99, .9]])
-# print(d)
+d = np.array([[50, 0, 4, 20],
+             [1.2, 104, 58, 2.0],
+             [1.3, 138, 99, .9]])
+print(d)
 
-# cal = d.sum(axis=0)
-# print(cal)
+cal = d.sum(axis=0)
+print(cal)
 
-# percentage = 100 * d/cal.reshape(1, 4)
-# print(percentage)
+percentage = 100 * d/cal.reshape(1, 4)
+print(percentage)
+
+a = np.random.rand(1000000)
+b = np.random.rand(1000000)
+
+tic = time.time()
+c = np.dot(a, b)
+toc = time.time()
+
+print(c)
+print("This took:" + str((toc - tic) * 1000) + "ms")
+
+c = 0
+tic = time.time()
+for i in range(1000000):
+    c += a[i]*b[i]
+toc = time.time()
+print(c)
+print("This took:" + str((toc - tic) * 1000) + "ms")
+
+d = np.array([[50, 0, 4, 20],
+             [1.2, 104, 58, 2.0],
+             [1.3, 138, 99, .9]])
+print(d)
+
+cal = d.sum(axis=0)
+print(cal)
+
+percentage = 100 * d/cal.reshape(1, 4)
+print(percentage)
 
 
